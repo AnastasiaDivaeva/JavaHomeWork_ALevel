b3ca5048c59353e006b53702771eef164f3f0946
anastasia <sekach.199855@gmail.com>
2023-08-25 13:12:50 +0300

    Added method main in class Vector

--- src/ua/divaeva/hw7/Vector.java
+++ src/ua/divaeva/hw7/Vector.java
@@ -72,7 +72,6 @@ public class Vector {
         public static void main(String[] args) {
             Vector vector1 = new Vector(10.0, 11.0, 4.0);
             Vector vector2 = new Vector(30.0, 31.0, 32.0);
-            System.out.println("Vector1");
             System.out.println("VectorLength: " + vector1.vectorLength());
             System.out.println("productOfVectors: " + vector1.productOfVectors(vector2));
             System.out.println("cosOfVectorsAngle: " + vector1.cosOfVectorsAngle(vector2));
